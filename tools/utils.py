1020acbec1c88480ef6d5525116917df696fe159
秦勇（qinyong） <60870424+qinyong123456@users.noreply.github.com>
2025-11-14 13:14:53 +0800

    Refactor get_feature_dinov3 for dynamic layer selection

--- tools/utils.py
+++ tools/utils.py
@@ -4,7 +4,9 @@ from utils import encode_text_with_prompt_ensemble
 
 def get_feature_dinov3(image_path, batch_img, device, Dino_model):
     with torch.no_grad():
-        layers = [5, 11, 17, 23]
+        depth = len(Dino_model.blocks)
+        idxs = [int(round((depth - 1) * k / (4 - 1))) for k in range(4)]
+        layers = sorted(set([max(0, min(depth - 1, i)) for i in idxs]))
         patch_tokens_dict = {i: [] for i in layers}
         cls_tokens_dict = {i: [] for i in layers}
 
@@ -83,14 +85,15 @@ def get_anomaly_map(clip_model, image_info, device, model, Dino_model):
 
         # cross-modal contrastive learning
         anomaly_map_cross_modal = 100 * patch_features @ adjusted_text_feature
-        anomaly_map_cross_modal = F.interpolate(anomaly_map_cross_modal.permute(0, 2, 1).view(-1, 2, 32, 32),
+        S = int((patch_tokens[i].shape[1]) ** 0.5)
+        anomaly_map_cross_modal = F.interpolate(anomaly_map_cross_modal.permute(0, 2, 1).view(-1, 2, S, S),
                                     size=512, mode='bilinear', align_corners=True)
         anomaly_map_cross_modal = torch.softmax(anomaly_map_cross_modal, dim=1)
         anomaly_maps_cross_modal.append(anomaly_map_cross_modal)
 
         # anomaly-aware calibration
         anomaly_awareness_cls_patch = 10 * patch_features @ cls_features.squeeze().unsqueeze(-1)
-        anomaly_awareness_cls_patch = F.interpolate(anomaly_awareness_cls_patch.permute(0, 2, 1).view(-1, 1, 32, 32),
+        anomaly_awareness_cls_patch = F.interpolate(anomaly_awareness_cls_patch.permute(0, 2, 1).view(-1, 1, S, S),
                                     size=512, mode='bilinear', align_corners=True)
         anomaly_awareness_cls_patch = torch.sigmoid(anomaly_awareness_cls_patch)
         anomaly_map.append(torch.cat([1 - anomaly_awareness_cls_patch, anomaly_awareness_cls_patch], dim=1))
